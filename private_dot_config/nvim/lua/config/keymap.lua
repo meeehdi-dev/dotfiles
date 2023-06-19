0b3509bffddcc0aebd6d728d250cde3ac5845a06
meeehdi <mehdi.mhiri@gmail.com>
2023-06-19 16:19:50 +0200

    feat(telescope): include hidden files

--- private_dot_config/nvim/lua/config/keymap.lua
+++ private_dot_config/nvim/lua/config/keymap.lua
@@ -2,12 +2,16 @@ local telescope = require("telescope.builtin")
 local tree = require("nvim-tree.api")
 
 vim.keymap.set("n", "<C-f>", function()
-  telescope.live_grep()
+  telescope.live_grep({
+    hidden = true,
+  })
 end)
 vim.keymap.set("n", "<C-p>", function()
   vim.fn.system("git rev-parse --is-inside-work-tree")
   if vim.v.shell_error == 0 then
-    telescope.git_files()
+    telescope.git_files({
+      hidden = true,
+    })
   else
     telescope.find_files({
       hidden = true,
