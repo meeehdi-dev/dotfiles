e3ab381c4d1065e1b4a174ffb0b5ba564e51e993
meeehdi <mehdi.mhiri@gmail.com>
2023-06-20 00:13:56 +0200

    feat(nvim): update vim options, remove defaults

--- private_dot_config/nvim/lua/config/vim.lua
+++ private_dot_config/nvim/lua/config/vim.lua
@@ -1,7 +1,4 @@
-vim.opt.autoindent = true
-vim.opt.backup = false
-vim.opt.breakindent = true
-vim.opt.cmdheight = 1
+vim.opt.cursorcolumn = true
 vim.opt.cursorline = true
 vim.opt.encoding = "utf-8"
 vim.opt.expandtab = true
@@ -9,13 +6,17 @@ vim.opt.fileencoding = "utf-8"
 vim.opt.hlsearch = true
 vim.opt.ignorecase = true
 vim.opt.laststatus = 2
+vim.opt.list = true
+vim.opt.listchars:append "eol:↴"
+vim.opt.listchars:append "space:⋅"
 vim.opt.mouse = "n"
 vim.opt.number = true
+vim.opt.numberwidth = 2
 vim.opt.relativenumber = true
 vim.opt.scrolloff = 10
-vim.opt.shell = "fish"
 vim.opt.shiftwidth = 2
-vim.opt.showcmd = true
+vim.opt.showmatch = true
+vim.opt.smartcase = true
 vim.opt.smartindent = true
 vim.opt.smarttab = true
 vim.opt.softtabstop = 2
@@ -23,11 +24,6 @@ vim.opt.splitbelow = true
 vim.opt.splitright = true
 vim.opt.tabstop = 2
 vim.opt.title = true
-vim.opt.wrap = false
-vim.opt.termguicolors = true
 vim.opt.updatetime = 100
-vim.opt.list = true
-vim.opt.listchars:append "space:⋅"
-vim.opt.listchars:append "eol:↴"
 
 vim.g.mapleader = " "
