5e278d594e5f25771e7223f27e033d4468b54f76
meeehdi <mehdi.mhiri@gmail.com>
2023-06-21 09:32:24 +0200

    feat(fish): start tmux on start

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -39,3 +39,11 @@ abbr yw "yarn workspace"
 # prompt
 starship init fish | source
 
+if status is-interactive
+and not set -q TMUX
+    if tmux has-session -t home
+	exec tmux attach-session -t home
+    else
+        tmux new-session -s home
+    end
+end
