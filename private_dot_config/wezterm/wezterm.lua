1d14c9d93f9d12fc2d86b75fc9998331e7d6e432
meeehdi <mehdi.mhiri@gmail.com>
2023-06-28 23:31:43 +0200

    feat(wezterm): remove padding

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -58,4 +58,11 @@ config.background = {
 config.font = wezterm.font("CaskaydiaCove Nerd Font Mono")
 config.font_size = 12.0
 
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+
 return config
