38aa12484042d11c494c114b08545546c5c9ebf8
meeehdi <mehdi.mhiri@gmail.com>
2023-06-30 10:57:27 +0200

    feat(fish): use unnamed tmux session

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -41,9 +41,5 @@ starship init fish | source
 
 if status is-interactive
 and not set -q TMUX
-    if tmux has-session -t home
-	exec tmux attach-session -t home
-    else
-        tmux new-session -s home
-    end
+  tmux new-session
 end
