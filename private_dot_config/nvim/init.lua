f3b721d119df2bc6ac826aa84657a0afc731b988
meeehdi <mehdi.mhiri@gmail.com>
2023-06-30 11:05:13 +0200

    fix(nvim): wsl clipboard + yank shortcut + format

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -42,22 +42,55 @@ vim.opt.updatetime = 100
 -- Leader
 vim.g.mapleader = " "
 
+-- Diagnostics
+vim.diagnostic.config({
+  underline = true,
+  update_in_insert = true,
+  virtual_text = false,
+  float = {
+    focusable = false,
+  },
+  severity_sort = true,
+})
+
+vim.fn.sign_define("DiagnosticSignError", { text = "", texthl = "DiagnosticSignError" })
+vim.fn.sign_define("DiagnosticSignWarn", { text = "", texthl = "DiagnosticSignWarn" })
+vim.fn.sign_define("DiagnosticSignInfo", { text = "", texthl = "DiagnosticSignInfo" })
+vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticSignHint" })
+
+-- WSL Clipboard
+if vim.fn.has("wsl") then
+  vim.g.clipboard = {
+    name = "WslClipboard",
+    copy = {
+      ["+"] = "clip.exe",
+      ["*"] = "clip.exe",
+    },
+    paste = {
+      ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+    cache_enabled = 0,
+  }
+  end
+vim.keymap.set("v", "<leader>y", "\"*y")
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
 -- Keymap
 vim.keymap.set("n", "<C-e>", function()
   require("nvim-tree.api").tree.toggle({
     find_file = true,
-    focus = false,
+    focus = true,
   })
 end)
-
-vim.keymap.set("x", "p", "\"_dP")
 
 vim.keymap.set("n", "<F5>", vim.cmd.UndotreeToggle)
 vim.keymap.set("n", "<C-t>", vim.cmd.TroubleToggle)
 vim.keymap.set("n", "<S-t>", vim.diagnostic.open_float)
 vim.keymap.set("n", "<leader>f", function()
-  local file = vim.fn.findfile(".prettierrc", ".;")
-  if file == "" then
+  local rc = vim.fn.findfile(".prettierrc", ".;")
+  local rcjson = vim.fn.findfile(".prettierrc.json", ".;")
+  if rc == "" and rcjson == "" then
     vim.lsp.buf.format()
   else
     vim.cmd.Prettier()
@@ -68,14 +101,7 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz", { remap = false })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { remap = false })
 vim.keymap.set("n", "<C-o>", "<C-o>zz", { remap = false })
 
-vim.keymap.set("n", "q", "<nop>", { remap = false })
-vim.keymap.set("n", "Q", "<nop>", { remap = false })
-vim.keymap.set("n", "<PageUp>", "<nop>", { remap = false })
-vim.keymap.set("n", "<PageDown>", "<nop>", { remap = false })
-vim.keymap.set("n", "<C-f>", "<nop>", { remap = false })
-vim.keymap.set("i", "<C-c>", "<nop>", { remap = false })
 vim.keymap.set("n", "<F3>", ":noh<CR>", { remap = false })
-
 vim.keymap.set("n", "<F2>", vim.lsp.buf.rename)
 vim.keymap.set("n", "gd", function()
   require("telescope.builtin").lsp_definitions()
@@ -92,19 +118,11 @@ vim.keymap.set("n", "<C-i>", function()
   vim.lsp.buf.execute_command({ command = "_typescript.organizeImports", arguments = { vim.api.nvim_buf_get_name(0) } })
 end)
 
-vim.diagnostic.config({
-  underline = true,
-  update_in_insert = true,
-  virtual_text = false,
-  float = {
-    focusable = false,
-  },
-  severity_sort = true,
-})
-
-vim.fn.sign_define('DiagnosticSignError', { text = '', texthl = 'DiagnosticSignError' })
-vim.fn.sign_define('DiagnosticSignWarn', { text = '', texthl = 'DiagnosticSignWarn' })
-vim.fn.sign_define('DiagnosticSignInfo', { text = '', texthl = 'DiagnosticSignInfo' })
-vim.fn.sign_define('DiagnosticSignHint', { text = '', texthl = 'DiagnosticSignHint' })
+vim.keymap.set("n", "q", "<nop>", { remap = false })
+vim.keymap.set("n", "Q", "<nop>", { remap = false })
+vim.keymap.set("n", "<PageUp>", "<nop>", { remap = false })
+vim.keymap.set("n", "<PageDown>", "<nop>", { remap = false })
+vim.keymap.set("n", "<C-f>", "<nop>", { remap = false })
+vim.keymap.set("i", "<C-c>", "<nop>", { remap = false })
 
 require("lazy").setup("plugins")
