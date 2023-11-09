8188ce93eb3e51e690e146da2b3698a81ca8efda
meeehdi <mehdi.mhiri@gmail.com>
2023-11-09 15:49:09 +0100

    fix(lualine): lsp clients by buf

--- private_dot_config/nvim/lua/plugins/lualine.lua
+++ private_dot_config/nvim/lua/plugins/lualine.lua
@@ -1,10 +1,11 @@
 local function lualine_lsp()
   local lsp = {}
-  local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
+  local buf_nr = vim.api.nvim_get_current_buf()
+  local buf_ft = vim.api.nvim_buf_get_option(buf_nr, "filetype")
   if buf_ft == "" then
     return "..."
   end
-  local clients = vim.lsp.get_active_clients()
+  local clients = vim.lsp.get_active_clients({ bufnr = buf_nr })
   if next(clients) ~= nil then
     for _, client in pairs(clients) do
       local filetypes = client.config.filetypes
