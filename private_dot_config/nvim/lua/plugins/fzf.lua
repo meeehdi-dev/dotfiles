6c030bc8ded670349fea290008b264673a96bcdc
meeehdi <mehdi.mhiri@gmail.com>
2024-01-02 11:54:14 +0100

    feat(nvim): replace telescope with fzf-lua

---
+++ private_dot_config/nvim/lua/plugins/fzf.lua
@@ -0,0 +1,50 @@
+return {
+  {
+    "ibhagwan/fzf-lua",
+    config = true,
+    keys = {
+      {
+        "<C-p>",
+        function()
+          vim.fn.system("git rev-parse --is-inside-work-tree")
+          local fzf = require("fzf-lua")
+          if vim.v.shell_error == 0 then
+            fzf.git_files()
+          else
+            fzf.files()
+          end
+        end,
+      },
+      {
+        "gd",
+        function()
+          require("fzf-lua").lsp_definitions()
+        end,
+      },
+      {
+        "gr",
+        function()
+          require("fzf-lua").lsp_references()
+        end,
+      },
+      {
+        "<C-f>",
+        function()
+          require("fzf-lua").live_grep()
+        end,
+      },
+      {
+        "<S-f>",
+        function()
+          require("fzf-lua").grep_cword()
+        end,
+      },
+      {
+        "<S-r>",
+        function()
+          require("fzf-lua").resume()
+        end,
+      },
+    },
+  },
+}
