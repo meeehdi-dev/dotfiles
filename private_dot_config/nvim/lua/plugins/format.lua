ee2d1db5d25227e0801e629ae4a8439855a9cbd6
meeehdi <mehdi.mhiri@gmail.com>
2024-04-16 20:41:17 +0200

    feat(nvim/format): use conform for json and remove shortcut

--- private_dot_config/nvim/lua/plugins/format.lua
+++ private_dot_config/nvim/lua/plugins/format.lua
@@ -1,46 +1,16 @@
-local json_ft = { "json", "jsonc" }
-local function format()
-  local formatted = require("conform").format()
-  if not formatted then
-    vim.lsp.buf.format()
-  end
-end
-
 return {
   {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
-        lua = { "stylua" },
         css = { "prettier" },
         javascript = { "prettier" },
         javascriptreact = { "prettier" },
+        json = { "jq" },
+        jsonls = { "jq" },
+        lua = { "stylua" },
         typescript = { "prettier" },
         typescriptreact = { "prettier" },
-      },
-    },
-    keys = {
-      {
-        "<leader>f",
-        function()
-          local buf_ft = vim.api.nvim_get_option_value("filetype", {})
-          if buf_ft == "json" then
-            vim.cmd(":%!jq '.'")
-          else
-            format()
-          end
-        end,
-      },
-      {
-        "<leader>fs",
-        function()
-          local buf_ft = vim.api.nvim_get_option_value("filetype", {})
-          if json_ft[buf_ft] then
-            vim.cmd(":%!jq --sort-keys '.'")
-          else
-            format()
-          end
-        end,
       },
     },
   },
