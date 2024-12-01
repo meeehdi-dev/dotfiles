3299a9df1b92affce176ffe7d99f57e956d290e3
meeehdi <mehdi.mhiri@gmail.com>
2024-12-01 08:17:11 +0100

    feat(nvim): disable bropilot

--- private_dot_config/nvim/lua/plugins/bropilot.lua
+++ private_dot_config/nvim/lua/plugins/bropilot.lua
@@ -1,11 +1,11 @@
 return {
-  {
-    "meeehdi-dev/bropilot.nvim",
-    event = "VeryLazy",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "j-hui/fidget.nvim",
-    },
-    opts = {},
-  },
+  -- {
+  --   "meeehdi-dev/bropilot.nvim",
+  --   event = "VeryLazy",
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",
+  --     "j-hui/fidget.nvim",
+  --   },
+  --   opts = {},
+  -- },
 }
