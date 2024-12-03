c6fb6f842909cc234f454ad3bf7d2dca5bfd46b9
meeehdi <mehdi.mhiri@gmail.com>
2024-12-03 22:19:46 +0100

    fix(nvim): cmp luasnip

--- private_dot_config/nvim/lua/plugins/lsp.lua
+++ private_dot_config/nvim/lua/plugins/lsp.lua
@@ -100,6 +100,7 @@ local handler_opts = {
 
 local function setup_handler(server_name)
   local opts = handler_opts[server_name] or {}
+  opts.capabilities = require("cmp_nvim_lsp").default_capabilities()
   local handler_on_attach = opts.on_attach
   opts.on_attach = function(client, bufnr)
     if handler_on_attach then
@@ -127,6 +128,7 @@ return {
   {
     "neovim/nvim-lspconfig",
     dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
       "williamboman/mason.nvim",
       "williamboman/mason-lspconfig.nvim",
     },
