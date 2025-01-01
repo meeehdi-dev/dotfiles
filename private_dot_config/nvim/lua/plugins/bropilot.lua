7086e47708e62f7d5ab6cbdfc3d0b724c1181faf
meeehdi <mehdi.mhiri@gmail.com>
2025-01-01 23:02:44 +0100

    feat(nvim): bring back bropilot with default options

--- private_dot_config/nvim/lua/plugins/bropilot.lua
+++ private_dot_config/nvim/lua/plugins/bropilot.lua
@@ -1,14 +1,10 @@
 return {
   {
-    enabled = false,
     "meeehdi-dev/bropilot.nvim",
     dependencies = {
       "nvim-lua/plenary.nvim",
       "j-hui/fidget.nvim",
     },
-    opts = {
-      auto_suggest = false,
-      debounce = 1,
-    },
+    opts = {},
   },
 }
