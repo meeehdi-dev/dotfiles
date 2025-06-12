e369f6db9e224c29aee5920c382afd2770d85c68
Mehdi <mehdi.mhiri@gmail.com>
2025-06-12 09:53:42 +0900

    feat(lsp): change config to handle mason v2 and new lsp configs

--- private_dot_config/nvim/lua/plugins/lsp.lua
+++ private_dot_config/nvim/lua/plugins/lsp.lua
@@ -1,72 +1,54 @@
-local handler_opts = {
-  cssls = {
-    settings = {
-      css = {
-        validate = true,
-        lint = {
-          unknownAtRules = "ignore",
-        },
+vim.lsp.config("cssls", {
+  settings = {
+    css = {
+      validate = true,
+      lint = {
+        unknownAtRules = "ignore",
       },
     },
   },
-  volar = {
-    settings = {
-      html = {
-        format = {
-          enable = false,
-        },
+})
+vim.lsp.config("vue_ls", {
+  settings = {
+    html = {
+      format = {
+        enable = false,
       },
     },
   },
-  ts_ls = {
-    filetypes = {
-      "javascript",
-      "javascriptreact",
-      "javascript.jsx",
-      "typescript",
-      "typescriptreact",
-      "typescript.jsx",
-      "vue",
-    },
-    init_options = {
-      plugins = {
-        {
-          name = "@vue/typescript-plugin",
-          location = vim.fn.expand(
-            "$HOME/.local/share/nvim/mason/packages/vue-language-server/node_modules/@vue/language-server"
-          ),
-          languages = { "vue" },
-        },
+})
+vim.lsp.config("ts_ls", {
+  filetypes = {
+    "javascript",
+    "javascriptreact",
+    "javascript.jsx",
+    "typescript",
+    "typescriptreact",
+    "typescript.jsx",
+    "vue",
+  },
+  init_options = {
+    plugins = {
+      {
+        name = "@vue/typescript-plugin",
+        location = vim.fn.stdpath("data")
+          .. "/mason/packages/vue-language-server/node_modules/@vue/language-server",
+        languages = { "vue" },
       },
     },
   },
-}
-
-local function setup_handler(server_name)
-  local opts = handler_opts[server_name] or {}
-  local capabilities = vim.tbl_deep_extend(
-    "force",
-    {},
-    vim.lsp.protocol.make_client_capabilities(),
-    require("blink.cmp").get_lsp_capabilities() or {},
-    opts.capabilities or {}
-  )
-  opts.capabilities = capabilities
-
-  require("lspconfig")[server_name].setup(opts)
-end
+})
 
 return {
   {
     "neovim/nvim-lspconfig",
     dependencies = {
-      "saghen/blink.cmp",
       {
-        "williamboman/mason.nvim",
+        "mason-org/mason.nvim",
         cmd = { "Mason" },
         opts = {},
       },
-      "williamboman/mason-lspconfig.nvim",
+      "mason-org/mason-lspconfig.nvim",
     },
     config = function()
       local mason = require("mason")
@@ -75,11 +57,7 @@ return {
       mason.setup()
       mason_lspconfig.setup({
         automatic_enable = true,
-        automatic_installation = false,
         ensure_installed = {},
-        handlers = {
-          setup_handler,
-        },
       })
     end,
   },
